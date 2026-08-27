73389c9897cb59240f538bc88ce46331673d0884
Jørgen Larsen <jorgen@larsen.zone>
2026-08-27 02:05:06 +0200

    Add PX7 Markdown Viewer cask

---
+++ Casks/px7-markdown-viewer.rb
@@ -0,0 +1,18 @@
+cask "px7-markdown-viewer" do
+  version "1.3.12"
+  sha256 "6800ef3c6dcde3abfbe81807fb651e4a0c518d2eab2927f6bd13ede84141ea89"
+
+  url "https://downloads.px7.digital/markdown/markdown-viewer/#{version}/PX7MarkdownViewer-#{version}.zip"
+  name "PX7 Markdown Viewer"
+  desc "Preview, open, and edit Markdown files locally"
+  homepage "https://markdown.px7.digital/markdown-viewer/"
+
+  depends_on macos: ">= :sonoma"
+
+  app "PX7 Markdown Viewer.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.px7.markdownviewer.plist",
+    "~/Library/Saved Application State/com.px7.markdownviewer.savedState",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,13 @@
+# PX7 Homebrew Tap
+
+Homebrew Casks for notarized PX7 Mac apps.
+
+## PX7 Markdown Viewer
+
+Preview Markdown files in Finder Quick Look, open them rendered, and edit the source locally.
+
+```sh
+brew install --cask px7digital/tap/px7-markdown-viewer
+```
+
+[Product page](https://markdown.px7.digital/markdown-viewer/)
